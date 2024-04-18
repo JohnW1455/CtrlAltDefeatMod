ad078ff53b2b313949bf8e69f5ca04aee9de77b4
Evan Yoshitani <evanyoshitani@gmail.com>
2024-04-18 03:39:10 -0400

    Dagger

--- CaveCultCode/Altar.cs
+++ CaveCultCode/Altar.cs
@@ -11,31 +11,5 @@ namespace CaveCultCode
 {
     public class Altar
     {
-        private IModHelper helper;
-
-        private Vector2 position;
-        public Vector2 Position
-        {
-            get { return position; }
-        }
-
-
-        public Altar(IModHelper helper) {
-            this.helper = helper;
-            this.position = new Vector2(21, 31);
-        }
-
-        public void activateAltar(CustomWeaponEnchantment customWeapon)
-        {
-            if(customWeapon.CurrentKills > 5)
-            {
-                customWeapon.CurrentKills = 0;
-                Game1.weatherForTomorrow = "weather_rain";
-            }
-            else
-            {
-                Game1.addHUDMessage(new HUDMessage("Hmmm doesn't seem to be working... I should come back when I've charged it up more.", 2)); 
-            }
-        }
     }
 }
--- CaveCultCode/obj/Debug/net6.0/CaveCultCode.AssemblyInfo.cs
+++ CaveCultCode/obj/Debug/net6.0/CaveCultCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaveCultCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa624906f6801ed9b4b626ac554d02fa8377887d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed688cf5f9b2c136375ca680ffa8e1996aa6f917")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaveCultCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaveCultCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
